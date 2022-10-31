a721842111930f3a133b435b251c4925974e25a2
Dino Morelli <dino@ui3.info>
2022-10-31 17:03:55 -0400

    Added some simple initial examples of LogAction in co-log

--- src/app/Main.hs
+++ src/app/Main.hs
@@ -1,9 +1,35 @@
--- It is generally a good idea to keep all your business logic in your library
--- and only use it in the executable. Doing so allows others to use what you
--- wrote in their libraries.
-import Example ( example )
+import Colog.Core (LogAction (..), (<&), logStringStdout)
+
+
+-- This is what the LogAction type looks like
+
+-- newtype LogAction m msg = LogAction
+--   { unlogAction :: msg -> m ()
+--   }
+
+
+example0 :: IO ()
+example0 = do
+  putStrLn "Example 0: First message"
+  putStrLn "Example 0: Second message"
+
+
+example1 :: LogAction IO String -> IO ()
+example1 logger' = do
+  unLogAction logger' "Example 1: First message"
+  unLogAction logger' "Example 1: Second message"
+
+-- (<&) is an operator version of unLogAction
+
+example2 :: LogAction IO String -> IO ()
+example2 logger' = do
+  logger' <& "Example 2: First message"
+  logger' <& "Example 2: Second message"
 
 
 main :: IO ()
 main = do
-  example
+  example0
+  let logger = logStringStdout
+  example1 logger
+  example2 logger
