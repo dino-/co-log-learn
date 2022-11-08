5170dfc5d18b38240c7b59067493170bc5937cc2
Dino Morelli <dino@ui3.info>
2022-11-08 00:37:23 -0500

    Added examples of using Severity and custom formatting
    This requires us to start using the more powerful co-log library instead
    of just co-log-core. To achieve this, we had to go back to Stackage LTS
    16.31 to find a snapshot where co-log hadn't yet been removed.

--- src/app/Main.hs
+++ src/app/Main.hs
@@ -1,4 +1,10 @@
-import Colog.Core (LogAction (..), (<&), logStringStdout)
+import Colog (cmap, logStringStdout)
+import Colog.Actions (logTextStdout)
+import Colog.Core.Action ((<&), LogAction, unLogAction)
+import Colog.Core.Severity (Severity(..))
+import Colog.Message (Message, Msg(..), fmtMessage, logDebug, logError, logInfo, logWarning, showSourceLoc)
+import Colog.Monad (LoggerT, usingLoggerT)
+import Data.Text (Text, pack)
 
 
 -- This is what the LogAction type looks like
@@ -10,26 +16,63 @@ import Colog.Core (LogAction (..), (<&), logStringStdout)
 
 example0 :: IO ()
 example0 = do
-  putStrLn "Example 0: First message"
-  putStrLn "Example 0: Second message"
+  putStrLn "Example 0: First message, simple putStrLn"
+  putStrLn "Example 0: Second message, simple putStrLn"
 
 
 example1 :: LogAction IO String -> IO ()
 example1 logger' = do
-  unLogAction logger' "Example 1: First message"
-  unLogAction logger' "Example 1: Second message"
+  unLogAction logger' "Example 1: First message, using unLogAction"
+  unLogAction logger' "Example 1: Second message, using unLogAction"
 
 -- (<&) is an operator version of unLogAction
 
 example2 :: LogAction IO String -> IO ()
 example2 logger' = do
-  logger' <& "Example 2: First message"
-  logger' <& "Example 2: Second message"
+  logger' <& "Example 2: First message, using the (<&) operator on the logger"
+  logger' <& "Example 2: Second message, using the (<&) operator on the logger"
+
+
+-- Here's a more practical example with log severities and even call stack info
+-- that's in the LoggerT monad transformer.
+
+-- First we use the stock fmtMessage and then use the following two functions
+-- to construct a custom logger with these features:
+--   - No color output
+--   - Only Debug shows the call stack info
+
+showSeverity' :: Severity -> Text
+showSeverity' Debug   = "[Debug]   "
+showSeverity' Info    = "[Info]    "
+showSeverity' Warning = "[Warning] "
+showSeverity' Error   = "[Error]   "
+
+fmtCustom :: Message -> Text
+fmtCustom (Msg sev@Debug stack msg) = showSeverity' sev <> showSourceLoc stack <> msg
+fmtCustom (Msg sev       _     msg) = showSeverity' sev <> msg
+
+showText :: Show a => a -> Text
+showText = pack . show
+
+exampleN :: Int -> LoggerT Message IO ()
+exampleN exNum = do
+  logDebug $ "example" <> showText exNum <> ": A debug message"
+  logInfo $ "example" <> showText exNum <> ": This is an info message"
+  logWarning $ "example" <> showText exNum <> ": A warning message"
+  logError $ "example" <> showText exNum <> ": An error message"
 
 
 main :: IO ()
 main = do
   example0
-  let logger = logStringStdout
-  example1 logger
-  example2 logger
+
+  let logger1 = logStringStdout
+
+  example1 logger1
+  example2 logger1
+
+  let logger2 = cmap fmtMessage logTextStdout
+  usingLoggerT logger2 $ exampleN 3
+
+  let logger3 = cmap fmtCustom logTextStdout
+  usingLoggerT logger3 $ exampleN 4
